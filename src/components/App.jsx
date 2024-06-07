8a8a894d9faca419fda0e7a739014e2aaa7f4ae8
Verbiturum <user@example.com>
2024-06-07 20:11:14 +0300

    Add functionality for adding new job form sections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,28 @@ import '../styles/App.css';
 import '../styles/reset.css';
 import FormSection from './FormSection.jsx';
 import Button from './Button.jsx';
+import { useState } from 'react';
 function App() {
+  const [jobFormSectionsNumber, setJobFormSectionsNumber] = useState(1);
+
+  function addNewJobFormSection() {
+
+    setJobFormSectionsNumber(jobFormSectionsNumber + 1);
+  }
+
+  const jobFormSections = [];
+
+  for (let i = 0; i < jobFormSectionsNumber; i++) {
+    jobFormSections.push(<FormSection headerText='Job' inputFields={['Job title', 'Company', 'Responsibilities', 'Date of work']} />);
+  }
 
   return (
     <div className="app">
       <h1>CV Application</h1>
       <FormSection headerText='Name' inputFields={['Name', 'Email', 'Phone']} />
       <FormSection headerText='Education' inputFields={['University', 'Speciality', 'Date of study']} />
-      <FormSection headerText='Previous jobs' inputFields={['Company', 'Position', 'Responsibilities', 'Date']} />
-      <Button name='Add new' type='button' />
+      {jobFormSections}
+      <Button name='Add new' type='button' onClick={addNewJobFormSection} />
       <Button name='Submit' type="submit" />
     </div>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import '../styles/Button.css';
 function Button(props) {
 
 	return (
-		<button type={props.type}>{props.name}</button>
+		<button type={props.type} onClick={props.onClick}>{props.name}</button>
 	);
 }
 
